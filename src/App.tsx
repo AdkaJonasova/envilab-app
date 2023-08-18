b08186c7e4238ea4e8472144a6168a932cec33cf
AdkaJonasova <xjonaso1@fi.muni.cz>
2023-08-18 13:08:00 +0200

    Added main menu component, added basic theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainMenu from './components/MainMenu';
 
 function App() {
   return (
     <div className="App">
+      <MainMenu />
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/utils/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material";
+import { ColorValueHex } from "./types";
+
+export const DarkGreen : ColorValueHex = '#59745D';
+export const LightGreen: ColorValueHex = '#8B958A';
+export const Beige: ColorValueHex = '#FEEDE0';
+export const Brown: ColorValueHex = '#E1BDA5';
+
+export const theme = () => {
+	return createTheme({
+		palette: {
+			primary: {
+                main: LightGreen,
+                dark: DarkGreen,
+            }
+		}
+	});
+};
+
