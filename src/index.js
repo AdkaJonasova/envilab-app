7f490dde71099c45a0a6046d0a7fb1e737368a18
AdkaJonasova <xjonaso1@fi.muni.cz>
2023-09-03 17:07:10 +0200

    Converted app to js, added map

--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from '@mui/material';
-import { theme } from './utils/theme';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
--- src/utils/data.ts
+++
@@ -1,4 +0,0 @@
-// Main menu
-export const PageName : string = "ENVILAB";
-export const SettingsButton : string = "Settings";
-export const ViewsButton : string = "Views";
