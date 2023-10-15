112dd41581284451f95bb41adf34f547bcc701bc
AdkaJonasova <xjonaso1@fi.muni.cz>
2023-10-15 20:55:01 +0200

    Changed searchbar to outlined input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,39 +1,30 @@
-import { Box, IconButton, TextField } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  InputAdornment,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import PropTypes from "prop-types";
 
 export default function SearchBar({ setFilter }) {
   return (
-    <form>
-      <Box
-        marginY={2}
-        marginX={2}
-        sx={{
-          backgroundColor: "red",
-          flexDirection: "row",
-          justifyContent: "flex-end",
+    <Box marginY={1} marginX={1}>
+      <OutlinedInput
+        type="search"
+        size="small"
+        fullWidth
+        onInput={(e) => {
+          setFilter(e.target.value);
         }}
-      >
-        <TextField
-          id="search-bar"
-          className="text"
-          onInput={(e) => {
-            setFilter(e.target.value);
-          }}
-          label="Enter name"
-          variant="outlined"
-          placeholder="Search"
-          size="small"
-        />
-        <IconButton
-          type="submit"
-          aria-label="search"
-          sx={{ justifyContent: "right", backgroundColor: "blue" }}
-        >
-          <SearchIcon />
-        </IconButton>
-      </Box>
-    </form>
+        endAdornment={
+          <InputAdornment position="end">
+            <SearchIcon />
+          </InputAdornment>
+        }
+      ></OutlinedInput>
+    </Box>
   );
 }
 
