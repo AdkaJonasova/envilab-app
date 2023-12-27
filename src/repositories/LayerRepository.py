2e1bb5417464e554933292de311a11afd99115f7
AdkaJonasova <xjonaso1@fi.muni.cz>
2023-12-27 17:33:34 +0100

    Added transaction commit, Added check if exists, Added remove from favorites and deactivate

--- src/repositories/LayerRepository.py
+++ src/repositories/LayerRepository.py
@@ -57,22 +57,61 @@ class LayerRepository:
         return cursor.fetchall()
 
     def activate_layer_for_user(self, layer_id, user_id):
+        layers = self.get_layer_by_id_and_user(layer_id, user_id)
+        if layers:
+            found_layer = layers[0]
+            self.__update_layer(layer_id, 'true', found_layer[2], user_id)
+        else:
+            self.__insert_layer(layer_id, 'true', 'true', user_id)
+
+        self.connection.commit()
+
+    def deactivate_layer_for_user(self, layer_id, user_id):
+        layers = self.get_layer_by_id_and_user(layer_id, user_id)
+        if layers:
+            found_layer = layers[0]
+            self.__update_layer(layer_id, 'false', found_layer[2], user_id)
+        else:
+            self.__insert_layer(layer_id, 'false', 'true', user_id)
+
+        self.connection.commit()
+
+    def add_layer_to_favorites_for_user(self, layer_id, user_id):
+        layers = self.get_layer_by_id_and_user(layer_id, user_id)
+        if layers:
+            found_layer = layers[0]
+            self.__update_layer(layer_id, found_layer[1], 'true', user_id)
+        else:
+            self.__insert_layer(layer_id, 'false', 'true', user_id)
+
+        self.connection.commit()
+
+    def remove_layer_from_favorites_for_user(self, layer_id, user_id):
+        layers = self.get_layer_by_id_and_user(layer_id, user_id)
+        if layers:
+            found_layer = layers[0]
+            self.__update_layer(layer_id, found_layer[1], 'false', user_id)
+        else:
+            self.__insert_layer(layer_id, 'false', 'false', user_id)
+
+        self.connection.commit()
+
+    # Private methods
+    def __insert_layer(self, layer_id, is_active, is_favorite, user_id):
         cursor = self.connection.cursor()
 
         user_layers = Table(self.TABLE_NAME)
-        query = (Query().update(user_layers)
-                 .set(user_layers.isActive, 'true')
-                 .where(user_layers.layerID == layer_id)
-                 .where(user_layers.userID == user_id))
+        query = Query().into(user_layers).insert(layer_id, is_active, is_favorite, user_id)
 
         cursor.execute(str(query))
 
-    def add_layer_to_favorites_for_user(self, layer_id, user_id):
+    def __update_layer(self, layer_id, is_active, is_favorite, user_id):
         cursor = self.connection.cursor()
 
         user_layers = Table(self.TABLE_NAME)
         query = (Query().update(user_layers)
-                 .set(user_layers.isFavorite, 'true')
+                 .set(user_layers.isFavorite, is_favorite)
+                 .set(user_layers.isActive, is_active)
                  .where(user_layers.layerID == layer_id)
                  .where(user_layers.userID == user_id))
 
