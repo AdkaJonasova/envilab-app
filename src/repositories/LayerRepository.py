65f22cbe5c68087136ff55e4d4ba69446b649b6e
AdkaJonasova <xjonaso1@fi.muni.cz>
2023-12-27 16:09:42 +0100

    Created LayerRepository.py and added method get favorite

---
+++ src/repositories/LayerRepository.py
@@ -0,0 +1,21 @@
+from pypika import Query, Table
+
+from src.utils.DatabaseUtil import connect
+
+
+class LayerRepository:
+    TABLE_NAME = "UserLayer"
+
+    def __init__(self):
+        self.connection = connect()
+
+    def get_all_favorite(self, user_id):
+        cursor = self.connection.cursor()
+
+        user_layers = Table(self.TABLE_NAME)
+        query = (Query().from_(user_layers)
+                 .select(user_layers.layerID, user_layers.isActive, user_layers.isFavorite, user_layers.userID)
+                 .where(user_layers.isFavorite == 'true'))
+
+        cursor.execute(str(query))
+        return cursor.fetchall()
