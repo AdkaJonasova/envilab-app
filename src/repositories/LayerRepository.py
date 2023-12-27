9a8284f750284b69c934c13f3a11a03fad34cab3
AdkaJonasova <xjonaso1@fi.muni.cz>
2023-12-27 16:33:23 +0100

    Added method get all by id and user

--- src/repositories/LayerRepository.py
+++ src/repositories/LayerRepository.py
@@ -9,24 +9,49 @@ class LayerRepository:
     def __init__(self):
         self.connection = connect()
 
-    def get_all_favorite(self, user_id):
+    def get_all_favorite_for_user(self, user_id):
         cursor = self.connection.cursor()
 
         user_layers = Table(self.TABLE_NAME)
         query = (Query().from_(user_layers)
                  .select(user_layers.layerID, user_layers.isActive, user_layers.isFavorite, user_layers.userID)
+                 .where(user_layers.userID == user_id)
                  .where(user_layers.isFavorite == 'true'))
 
         cursor.execute(str(query))
         return cursor.fetchall()
 
-    def get_all_active(self, user_id):
+    def get_all_active_for_user(self, user_id):
         cursor = self.connection.cursor()
 
         user_layers = Table(self.TABLE_NAME)
         query = (Query().from_(user_layers)
                  .select(user_layers.layerID, user_layers.isActive, user_layers.isFavorite, user_layers.userID)
+                 .where(user_layers.userID == user_id)
                  .where(user_layers.isActive == 'true'))
+
+        cursor.execute(str(query))
+        return cursor.fetchall()
+
+    def get_all_for_user(self, user_id):
+        cursor = self.connection.cursor()
+
+        user_layers = Table(self.TABLE_NAME)
+        query = (Query().from_(user_layers)
+                 .select(user_layers.layerID, user_layers.isActive, user_layers.isFavorite, user_layers.userID)
+                 .where(user_layers.userID == user_id))
+
+        cursor.execute(str(query))
+        return cursor.fetchall()
+
+    def get_layer_by_id_and_user(self, layer_id, user_id):
+        cursor = self.connection.cursor()
+
+        user_layers = Table(self.TABLE_NAME)
+        query = (Query().from_(user_layers)
+                 .select(user_layers.layerID, user_layers.isActive, user_layers.isFavorite, user_layers.userID)
+                 .where(user_layers.layerID == layer_id)
+                 .where(user_layers.userID == user_id))
 
         cursor.execute(str(query))
         return cursor.fetchall()
