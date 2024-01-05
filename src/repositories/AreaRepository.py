7376cab06c3703c481f568d2a860df50add9d586
AdkaJonasova <xjonaso1@fi.muni.cz>
2024-01-05 10:28:48 +0100

    Implemented (de)active, add/remove favorite, add/remove custom methods for AreaRepository.py

--- src/repositories/AreaRepository.py
+++ src/repositories/AreaRepository.py
@@ -10,6 +10,18 @@ class AreaRepository:
     def __int__(self):
         self.connection = connect()
 
+    def get_area_by_id_and_user(self, area_id, user_id):
+        cursor = self.connection.cursor()
+
+        user_areas = Table(self.AREA_TABLE_NAME)
+        query = (Query().from_(user_areas)
+                 .select(user_areas.areaID, user_areas.isActive, user_areas.isFavorite, user_areas.isCustom, user_areas.userID)
+                 .where(user_areas.areaID == area_id)
+                 .where(user_areas.userID == user_id))
+
+        cursor.execute(str(query))
+        return cursor.fetchall()
+
     def get_areas_for_user(self, user_id):
         cursor = self.connection.cursor()
 
@@ -60,23 +72,65 @@ class AreaRepository:
         cursor.execute(str(query))
         return cursor.fetchall()
 
-    def activate_area_for_user(self, user_id, layer_id):
-        return
+    def activate_area_for_user(self, user_id, area_id):
+        areas = self.get_area_by_id_and_user(area_id, user_id)
+        if areas:
+            found_area = areas[0]
+            self.__update_area_for_user(user_id, area_id, 'true', found_area[2], found_area[3])
+        else:
+            self.__create_area_for_user(user_id, area_id, 'true', 'false', 'false')
 
-    def deactivate_area_for_user(self, user_id, layer_id):
-        return
+        self.connection.commit()
 
-    def add_favorite_for_user(self, user_id, layer_id):
-        return
+    def deactivate_area_for_user(self, user_id, area_id):
+        areas = self.get_area_by_id_and_user(area_id, user_id)
+        if areas:
+            found_area = areas[0]
+            self.__update_area_for_user(user_id, area_id, 'false', found_area[2], found_area[3])
+        else:
+            self.__create_area_for_user(user_id, area_id, 'false', 'false', 'false')
 
-    def remove_favorite_for_user(self, user_id, layer_id):
-        return
+        self.connection.commit()
 
-    def add_custom_for_user(self, user_id, layer_id):
-        return
+    def add_favorite_for_user(self, user_id, area_id):
+        areas = self.get_area_by_id_and_user(area_id, user_id)
+        if areas:
+            found_area = areas[0]
+            self.__update_area_for_user(user_id, area_id, found_area[1], 'true', found_area[3])
+        else:
+            self.__create_area_for_user(user_id, area_id, 'false', 'true', 'false')
 
-    def remove_custom_for_user(self, user_id, layer_id):
-        return
+        self.connection.commit()
+
+    def remove_favorite_for_user(self, user_id, area_id):
+        areas = self.get_area_by_id_and_user(area_id, user_id)
+        if areas:
+            found_area = areas[0]
+            self.__update_area_for_user(user_id, area_id, found_area[1], 'false', found_area[3])
+        else:
+            self.__create_area_for_user(user_id, area_id, 'false', 'false', 'false')
+
+        self.connection.commit()
+
+    def add_custom_for_user(self, user_id, area_id):
+        areas = self.get_area_by_id_and_user(area_id, user_id)
+        if areas:
+            found_area = areas[0]
+            self.__update_area_for_user(user_id, area_id, found_area[1], found_area[2], 'true')
+        else:
+            self.__create_area_for_user(user_id, area_id, 'false', 'false', 'true')
+
+        self.connection.commit()
+
+    def remove_custom_for_user(self, user_id, area_id):
+        areas = self.get_area_by_id_and_user(area_id, user_id)
+        if areas:
+            found_area = areas[0]
+            self.__update_area_for_user(user_id, area_id, found_area[1], found_area[2], 'false')
+        else:
+            self.__create_area_for_user(user_id, area_id, 'false', 'false', 'true')
+
+        self.connection.commit()
 
     # Private methods
     def __create_area_for_user(self, user_id, area_id, is_active, is_favorite, is_custom):
