4b4a2be73c90df7d42605fd1e6cc4fabb7c665ad
AdkaJonasova <xjonaso1@fi.muni.cz>
2024-01-06 10:30:24 +0100

    Implemented create, add/remove from set for AreaSetRepository.py

--- src/repositories/AreaSetRepository.py
+++ src/repositories/AreaSetRepository.py
@@ -49,10 +49,39 @@ class AreaSetRepository:
         return cursor.fetchall()
 
     def add_area_to_set(self, area_id, area_set_id):
-        return
+        cursor = self.connection.cursor()
+
+        areas_in_sets = Table(self.AREA_IN_SET_TABLE_NAME)
+        query = Query().into(areas_in_sets).insert(area_id, area_set_id)
+
+        cursor.execute(str(query))
+        self.connection.commit()
 
     def remove_area_from_set(self, area_id, area_set_id):
-        return
+        cursor = self.connection.cursor()
+
+        areas_in_sets = Table(self.AREA_IN_SET_TABLE_NAME)
+        query = (Query()
+                 .from_(areas_in_sets)
+                 .where(areas_in_sets.areaID == area_id)
+                 .where(areas_in_sets.areaSetID == area_set_id)
+                 .delete())
+
+        cursor.execute(str(query))
+        self.connection.commit()
 
     def create_area_set(self, area_set_name, areas):
-        return
+        cursor = self.connection.cursor()
+
+        area_sets, areas_in_sets = Table(self.AREA_SET_TABLE_NAME, self.AREA_IN_SET_TABLE_NAME)
+
+        area_set_query = Query().into(area_sets).insert(area_set_name)
+        cursor.execute(str(area_set_query))
+        inserted_area_set_id = cursor.lastrowid
+
+        for area in areas:
+            area_query = Query().into(areas_in_sets).insert(area, inserted_area_set_id)
+            cursor.execute((str(area_query)))
+
+        self.connection.commit()
+
