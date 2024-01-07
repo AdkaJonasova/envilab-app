b6fa848ffc92b5b5ab1c4b1831a055513831689b
AdkaJonasova <xjonaso1@fi.muni.cz>
2024-01-07 17:13:58 +0100

    Added layer hooks

--- src/hooks/layerHooks.js
+++ src/hooks/layerHooks.js
@@ -1,12 +1,42 @@
 import { useQuery } from "react-query";
 import apiClient from "../http-common";
 
-export const useFavoriteLayers = () =>
+export const useFavoriteLayers = (userId) =>
   useQuery("favoriteLayers", () =>
-    fetch("http://localhost:8000/layers/favorite/1")
+    apiClient.get(`/layers/favorite/${userId}`).then((r) => r.data)
   );
 
-export const useActiveLayers = () =>
+export const useActiveLayers = (userId) =>
   useQuery("activeLayers", () =>
-    apiClient.get("/layers/active/1").then((r) => r.data)
+    apiClient.get(`/layers/active/${userId}`).then((r) => r.data)
+  );
+
+export const userLayers = (userId) =>
+  useQuery("layers", () =>
+    apiClient.get(`/layers/${userId}`).then((r) => r.data)
+  );
+
+export const useLayerById = (userId, layerId) =>
+  useQuery("layerById", () =>
+    apiClient.get(`/layers/${userId}/${layerId}`).then((r) => r.data)
+  );
+
+export const activateLayer = (userId, layerId) =>
+  useQuery("activateLayer", () =>
+    apiClient.post(`/layers/activate/${userId}/${layerId}`)
+  );
+
+export const deactivateLayer = (userId, layerId) =>
+  useQuery("deactivateLayer", () =>
+    apiClient.post(`/layers/deactivate/${userId}/${layerId}`)
+  );
+
+export const addFavoriteLayer = (userId, layerId) =>
+  useQuery("addFavoriteLayer", () =>
+    apiClient.post(`/layers/addFavorite/${userId}/${layerId}`)
+  );
+
+export const removeFavoriteLayer = (userId, layerId) =>
+  useQuery("removeFavoriteLayer", () =>
+    apiClient.post(`/layers/removeFavorite/${userId}/${layerId}`)
   );
