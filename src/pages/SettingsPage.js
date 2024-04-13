6ab89c3d672a728f8102e7d5e7ce2c7dadb14a77
AdkaJonasova <xjonaso1@fi.muni.cz>
2024-04-13 11:27:38 +0200

    Added data loading into SettingsPage if necessary

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,11 +1,60 @@
 import { Divider, Grid } from "@mui/material";
 import SideMenu from "../components/SideMenu";
 import { settingsPageMapping } from "../utils/data";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchLayerGroups,
+  selectLayersError,
+  selectLayersStatus,
+} from "../redux/slices/LayersSlice";
+import {
+  fetchAreas,
+  selectAreasError,
+  selectAreasStatus,
+} from "../redux/slices/AreasSlice";
+import { useEffect } from "react";
+import { FetchStates } from "../utils/enums";
+import { userId } from "../data/mockData";
+import Loading from "../components/global/Loading";
+import ErrorWindow from "../components/global/ErrorWindow";
 
 const SettingsPage = ({ tab }) => {
-  function getPageByTab() {
-    return settingsPageMapping.get(tab);
+  const layersStatus = useSelector(selectLayersStatus);
+  const layersError = useSelector(selectLayersError);
+  const areasStatus = useSelector(selectAreasStatus);
+  const areasError = useSelector(selectAreasError);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (layersStatus === FetchStates.Idle) {
+      dispatch(fetchLayerGroups(userId));
+    }
+    if (areasStatus === FetchStates.Idle) {
+      dispatch(fetchAreas(userId));
+    }
+  }, [dispatch, layersStatus, areasStatus]);
+
+  if (
+    layersStatus === FetchStates.Idle ||
+    layersStatus === FetchStates.Loading ||
+    areasStatus === FetchStates.Idle ||
+    areasStatus === FetchStates.Loading
+  ) {
+    return <Loading />;
   }
+
+  if (layersStatus === FetchStates.Failed) {
+    return <ErrorWindow errorMessage={layersError} />;
+  }
+
+  if (areasStatus === FetchStates.Failed) {
+    return <ErrorWindow errorMessage={areasError} />;
+  }
+
+  const getPageByTab = () => {
+    return settingsPageMapping.get(tab);
+  };
 
   return (
     <div>
